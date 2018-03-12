bfb8c33e1ef841636bd691528fd17eb74faddd8b
Router1 <Router1@Router1-PC>
2018-03-12 05:15:13 +0100

    Cambio de clase

--- src/banco2/Banco1Dam.java
+++ src/banco2/Banco1Dam.java
@@ -10,24 +10,15 @@ package banco2;
  * @author Router1
  */
 public class Banco1Dam {
-    
-    Clientes cliente = new Clientes();
-    
+      
     
         public static void main(String[] args) {
+          
+                 for (int i = 0; i < 30; i++) {
+             Clientes [] cliente = new Clientes[i]; 
+            
+            }         
+        
+        } 
 
-  Banco2 b = new Banco2("TubbiesBank", 5);
-  int nPo = b.abrirCuenta("Po", 500);
-  int nDixy = b.abrirCuenta("Dixy", 500);
-  int nTinkyWinky = b.abrirCuenta("Tinky Winky", 500);
-  int nLala = b.abrirCuenta("Lala", 500);
-  System.out.println(b);
-  b.ingreso(nPo, 100);
-  b.debito(nDixy, 100);
-  b.transferencia(nTinkyWinky, nLala, 100);
-  System.out.println(b);
-  b.cerrarCuenta(nPo);
-  b.ingreso(nDixy,200);
-  System.out.println(b);
-    }
 }
